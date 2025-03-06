ef8d71a197f9be633de4e0fa9a0f408732654190
Borys35 <boryskac10@gmail.com>
2025-03-06 23:03:10 +0100

    add other class files

---
+++ FSM.h
@@ -0,0 +1,8 @@
+//
+// Created by borys on 06/03/2025.
+//
+
+#ifndef FSM_H
+#define FSM_H
+
+#endif //FSM_H
---
+++ WashingMachine.h
@@ -0,0 +1,8 @@
+//
+// Created by borys on 06/03/2025.
+//
+
+#ifndef WASHINGMACHINE_H
+#define WASHINGMACHINE_H
+
+#endif //WASHINGMACHINE_H
